2a7ca5d8635588e04621b167f2bf7d9afb8777fb
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-17 20:32:22 -0400

    refactor: Removed unnecessary return values from PrepareCtx function

--- server/grpc/context/prepare.go
+++ server/grpc/context/prepare.go
@@ -6,17 +6,17 @@ import (
 )
 
 // PrepareCtx prepares the context for the gRPC request
-func PrepareCtx(ctx *gin.Context, request *any) (*gin.Context, context.Context, *any, error) {
+func PrepareCtx(ctx *gin.Context, request *any) (context.Context, error) {
 	// Bind the request
 	if err := ctx.ShouldBindJSON(request); err != nil {
-		return nil, nil, nil, err
+		return nil, err
 	}
 
 	// Get the outgoing context
 	grpcCtx, err := GetOutgoingCtx(ctx)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, err
 	}
 
-	return ctx, grpcCtx, request, nil
+	return grpcCtx, nil
 }
