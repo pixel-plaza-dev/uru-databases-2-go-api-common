83bc1dbab1a6852b8db529e002089d0a8684450c
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-17 22:53:15 -0400

    refactor: Modified GetOutgoingCtx function

--- server/grpc/context/metadata.go
+++ server/grpc/context/metadata.go
@@ -2,9 +2,11 @@ package context
 
 import (
 	"context"
+	"errors"
 	"github.com/gin-gonic/gin"
 	jwtmiddleware "github.com/pixel-plaza-dev/uru-databases-2-go-api-common/server/gin/middleware/jwt"
 	commongrpc "github.com/pixel-plaza-dev/uru-databases-2-go-service-common/server/grpc"
+	clientauthinterceptor "github.com/pixel-plaza-dev/uru-databases-2-go-service-common/server/grpc/client/interceptor/auth"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -13,6 +15,10 @@ func GetOutgoingCtx(ctx *gin.Context) (context.Context, error) {
 	// Get the token string from the context
 	token, err := jwtmiddleware.GetCtxTokenString(ctx)
 	if err != nil {
+		// Check if the token is missing
+		if errors.Is(err, clientauthinterceptor.MissingTokenError) {
+			return context.Background(), nil
+		}
 		return nil, err
 	}
 
