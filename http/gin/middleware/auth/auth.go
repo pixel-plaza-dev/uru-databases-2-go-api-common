fbcc7a5da92f371e8b73e50f4cc665aecb13a91f
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-11-20 17:03:03 -0400

    refactor: Modified the Auth middleware Authenticate handler

--- http/gin/middleware/auth/auth.go
+++ http/gin/middleware/auth/auth.go
@@ -62,24 +62,6 @@ func NewMiddleware(
 // Authenticate return the middleware function that authenticates the request
 func (m *Middleware) Authenticate() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// Get the authorization from the header
-		authorization := ctx.GetHeader(commongin.AuthorizationHeaderKey)
-
-		// Check if the authorization is a bearer token
-		parts := strings.Split(authorization, " ")
-
-		// Return an error if the authorization is missing or invalid
-		if len(parts) < 2 || parts[0] != commongin.BearerPrefix {
-			ctx.JSON(
-				401, gin.H{"error": InvalidAuthorizationHeaderError.Error()},
-			)
-			ctx.Abort()
-			return
-		}
-
-		// Get the token from the header
-		tokenString := parts[1]
-
 		// Get the full endpoint and method
 		fullRestEndpoint := ctx.FullPath()
 		restMethod := ctx.Request.Method
@@ -112,6 +94,30 @@ func (m *Middleware) Authenticate() gin.HandlerFunc {
 			ctx.Abort()
 			return
 		}
+
+		// Check if there is None interception
+		if interception == pbdetails.None {
+			ctx.Next()
+			return
+		}
+
+		// Get the authorization from the header
+		authorization := ctx.GetHeader(commongin.AuthorizationHeaderKey)
+
+		// Check if the authorization is a bearer token
+		parts := strings.Split(authorization, " ")
+
+		// Return an error if the authorization is missing or invalid
+		if len(parts) < 2 || parts[0] != commongin.BearerPrefix {
+			ctx.JSON(
+				401, gin.H{"error": InvalidAuthorizationHeaderError.Error()},
+			)
+			ctx.Abort()
+			return
+		}
+
+		// Get the token from the header
+		tokenString := parts[1]
 
 		// Validate the token and get the validated claims
 		claims, err := m.validator.GetValidatedClaims(tokenString, interception)
