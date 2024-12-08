fd76bd345c6860d2f4325bedf7495d447947f5be
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-08 09:15:33 -0400

    refactor: Improved gRPC response handler implementation
    * Improved gRPC response handler implementation
    * Upgraded dependencies

--- http/grpc/client/context/prepare.go
+++ http/grpc/client/context/prepare.go
@@ -4,33 +4,26 @@ import (
 	"context"
 	"errors"
 	"github.com/gin-gonic/gin"
-	commonclientresponse "github.com/pixel-plaza-dev/uru-databases-2-go-api-common/http/grpc/client/response"
 	"io"
 )
 
 // PrepareCtx prepares the context for the gRPC request
-func PrepareCtx(ctx *gin.Context, request interface{}, handler commonclientresponse.Handler) (
+func PrepareCtx(ctx *gin.Context, request interface{}) (
 	grpcCtx context.Context,
 	err error,
 ) {
-	// Check if the handler is nil
-	if handler == nil {
-		return nil, commonclientresponse.NilHandlerError
-	}
-
 	// Bind the request
 	if request != nil {
 		err = ctx.ShouldBindJSON(request)
 		if err != nil && !errors.Is(err, io.EOF) {
-			return nil, handler.HandlePrepareCtxError(err)
+			return nil, err
 		}
 	}
 
 	// Get the outgoing context
 	grpcCtx, err = GetOutgoingCtx(ctx)
 	if err != nil {
-		return nil,
-			handler.HandlePrepareCtxError(err)
+		return nil, err
 	}
 
 	return grpcCtx, nil
--- http/grpc/client/response/handler.go
+++ http/grpc/client/response/handler.go
@@ -1,16 +1,20 @@
 package response
 
 import (
+	"github.com/gin-gonic/gin"
 	commongin "github.com/pixel-plaza-dev/uru-databases-2-go-api-common/http/gin"
+	commongintypes "github.com/pixel-plaza-dev/uru-databases-2-go-api-common/http/gin/types"
 	commonflag "github.com/pixel-plaza-dev/uru-databases-2-go-service-common/config/flag"
-	commonclienterror "github.com/pixel-plaza-dev/uru-databases-2-go-service-common/http/grpc/client/error"
+	commonclientstatus "github.com/pixel-plaza-dev/uru-databases-2-go-service-common/http/grpc/client/status"
+	"google.golang.org/grpc/codes"
+	"net/http"
 )
 
 type (
 	// Handler interface
 	Handler interface {
-		HandlePrepareCtxError(err error) error
-		HandleStatusError(err error) error
+		HandlePrepareCtxError(ctx *gin.Context, err error)
+		HandleResponse(ctx *gin.Context, code int, response interface{}, err error)
 	}
 
 	// DefaultHandler struct
@@ -29,14 +33,42 @@ func NewDefaultHandler(mode *commonflag.ModeFlag) (*DefaultHandler, error) {
 }
 
 // HandlePrepareCtxError handles the prepare context error
-func (d DefaultHandler) HandlePrepareCtxError(err error) error {
-	if d.mode.IsDev() {
-		return err
+func (d DefaultHandler) HandlePrepareCtxError(ctx *gin.Context, err error) {
+	if d.mode.IsProd() {
+		err = commongin.InternalServerError
 	}
-	return commongin.InternalServerError
+
+	ctx.JSON(http.StatusInternalServerError, commongintypes.NewErrorResponse(err))
 }
 
-// HandleStatusError handles the status error
-func (d DefaultHandler) HandleStatusError(err error) error {
-	return commonclienterror.ExtractErrorFromStatus(d.mode, err)
+// HandleResponse handles the response from the gRPC server
+func (d DefaultHandler) HandleResponse(ctx *gin.Context, code int, response interface{}, err error) {
+	// Check if the error is nil
+	if err == nil {
+		ctx.JSON(code, response)
+		return
+	}
+
+	// Extract the gRPC code and error from the status
+	extractedCode, extractedErr := commonclientstatus.ExtractErrorFromStatus(d.mode, err)
+
+	// Check the extracted code and error
+	switch extractedCode {
+	case codes.AlreadyExists:
+		ctx.JSON(http.StatusConflict, commongintypes.NewErrorResponse(extractedErr))
+	case codes.NotFound:
+		ctx.JSON(http.StatusNotFound, commongintypes.NewErrorResponse(extractedErr))
+	case codes.InvalidArgument:
+		ctx.JSON(http.StatusBadRequest, commongintypes.NewErrorResponse(extractedErr))
+	case codes.PermissionDenied:
+		ctx.JSON(http.StatusForbidden, commongintypes.NewErrorResponse(extractedErr))
+	case codes.Unauthenticated:
+		ctx.JSON(http.StatusUnauthorized, commongintypes.NewErrorResponse(extractedErr))
+	case codes.Unimplemented:
+		ctx.JSON(http.StatusNotImplemented, commongintypes.NewErrorResponse(extractedErr))
+	case codes.Unavailable:
+		ctx.JSON(http.StatusServiceUnavailable, commongintypes.NewErrorResponse(extractedErr))
+	default:
+		ctx.JSON(http.StatusInternalServerError, commongintypes.NewErrorResponse(extractedErr))
+	}
 }
