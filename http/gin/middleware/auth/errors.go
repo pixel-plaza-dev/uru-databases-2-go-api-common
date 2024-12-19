f2c8ad157878301f6d89e9b75ce530adc9148dbf
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-19 15:02:37 -0400

    BREAKING CHANGE: Forked this repository into 'github.com/ralvarezdev/go-jwt' and 'github.com/ralvarezdev/go-gin'

--- http/gin/middleware/auth/errors.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-import "errors"
-
-var (
-	InvalidAuthorizationHeaderError = errors.New("invalid authorization header")
-	NilMapperError                  = errors.New("mapper cannot be nil")
-	NilGRPCInterceptionsError       = errors.New("grpc interceptions cannot be nil")
-)
--- http/gin/route/errors.go
+++
@@ -1,5 +0,0 @@
-package route
-
-var (
-	MissingGRPCMethodError = "missing gRPC method: %v"
-)
